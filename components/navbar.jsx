8f594b418c069d16c61bc6778c4fdc60db943436
willcodeankit <ankit.11122006@gmail.com>
2026-09-10 01:47:15 +0530

    Updating navbar

--- components/navbar.jsx
+++ components/navbar.jsx
@@ -1,8 +1,16 @@
-function navbar(){
+function Navbar(){
     return (
         <nav className="nav">
-            <div className="navbar"></div>
+            <div className="navbar">
+                <img src="/expense-logo.png" alt="expense logo"/>
+                <h1>Expense Tracker</h1>
+            </div>
+            <ul>
+                <li>About</li>
+                <li>Contact</li>
+            </ul>
 
         </nav>
     )
 }
+export default Navbar;
--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import { useState } from 'react'
-import './App.css'
+import Navbar from './components/Navbar.jsx'
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return 
-  
+  return (
+  <>
+  <Navbar/>
+  </>
+  )
 }
 
 export default App
