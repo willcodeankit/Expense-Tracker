5f20d5195fa69ff2e52121204bc5e0dd493348ae
willcodeankit <ankit.11122006@gmail.com>
2026-09-10 11:15:28 +0530

    Adding background to webpage

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Transaction from './components/Transaction';
 import History from './components/History';
 import Footer from './components/Footer';
 import { useTransaction } from './hooks/usetransaction';
+import './App.css'
 
 function App() {
   const {
@@ -16,13 +17,13 @@ function App() {
   } = useTransaction();
 
   return (
-    <>
+    <div className="app-container">
       <Navbar />
       <Dashboard income={totalIncome} expense={totalExpense} balance={balance} />
       <Transaction onAddTransaction={addTransaction} />
       <History transactions={transactions} onDelete={deleteTransaction} />
       <Footer />
-    </>
+    </div>
   );
 }
 
